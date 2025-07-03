ba1b5339d9b2fe3500d04181226f136e8facdff6
Nakul Arya <aryanakul31@gmail.com>
2025-07-03 13:56:31 +0530

    Login API and role based user fetch API

---
+++ node/src/app.js
@@ -0,0 +1,37 @@
+require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+// Middleware
+const errorHandler = require('./middleware/errorHandler');
+
+// Routes
+const userRoutes = require('./routes/users');
+const auth = require('./routes/auth');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors({
+    origin: `http://localhost:${PORT}`, // allow only this origin
+    methods: ['GET', 'POST'],
+    credentials: true,
+}));
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(errorHandler);
+
+// Routes
+app.use('/api/users', userRoutes);
+app.use('/api/auth', auth);
+
+app.get('/', (req, res) => {
+    res.send('Server is running');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}`);
+});
