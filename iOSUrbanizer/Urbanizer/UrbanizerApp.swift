8ccf63b49b721250189be86090dc89bd65fb7db8
Nakul Arya <aryanakul31@gmail.com>
2025-07-04 17:01:51 +0530

    iOS ci/cd

--- iOSUrbanizer/Urbanizer/UrbanizerApp.swift
+++ iOSUrbanizer/Urbanizer/UrbanizerApp.swift
@@ -5,10 +5,21 @@
 //  Created by Nakul Arya on 04/07/25.
 //
 
+import FirebaseCore
 import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 @main
 struct UrbanizerApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
